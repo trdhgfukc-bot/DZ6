9b3bc95223652ad508528f01e5b95caba67a9020
S <trdhgfukc@gmail.com>
2025-10-21 19:36:45 +0700

    добавлен класс car

---
+++ Car.java
@@ -0,0 +1,60 @@
+public class Car{
+    private int id;
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private int price;
+    private String rgNum;
+}
+public Car(int id, String brand, String model,int year, String color, int price, String rgNum){
+    this.id = id;
+    this.brand = brand;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.price = price;
+    this.rgNum = rgNum;
+}
+public setId(){
+    return id;
+}
+public void getId(){
+    this.id = id;
+}
+public void setBrand(){
+    return brand;
+}
+public void getBrand(){
+    this.brand = brand;
+}
+public void setModel(){
+    return model;
+}
+public void getModel(){
+    this.model = model;
+}
+public void setYear(){
+    return year;
+}
+public void getYear(){
+    this.year = year;
+}
+public void setColor(){
+    return color;
+}
+public void getColor(){
+    this.color = color;
+}
+public void getPrice(){
+    this.price = price;
+}
+public void setPrice(){
+    return price;
+}
+public void setRGNum(){
+    return rgNum;
+}
+public void getRGNum(){
+    this.rgNum = rgNum;
+}
