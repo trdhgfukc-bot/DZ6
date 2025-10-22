f4903482996ccfc10a869b3a5fef2b8ee4452405
S <trdhgfukc@gmail.com>
2025-10-23 00:05:13 +0700

    добавлен getcarbybrandandyearoptional

--- Main.java
+++ Main.java
@@ -2,16 +2,22 @@ public class Main {
     public static void main(String[] args) {
         Car[] cars = {
             new Car(1, "Toyota", "Camry", 2020, "Black", 25000, "AB123CD"),
-            new Car(2, "Honda", "Civic", 2019, "White", 22000, "EF456GH"),
+            new Car(2, "Audi", "Civic", 2019, "White", 22000, "EF456GH"),
             new Car(3, "Toyota", "Corolla", 2021, "Red", 20000, "IJ789KL"),
             new Car(4, "BMW", "X5", 2018, "Blue", 45000, "MN012OP")
         };
 
         Car[] toyotaCars = getCarByBrend(cars, "Toyota");
         System.out.println("Toyota cars found: " + toyotaCars.length);
+        for (Car car : toyotaCars) {
+            System.out.println(" - " + car.getBrand() + " " + car.getModel() + " " + car.getYear());}
         
         Car[] audiCars = getCarByBrend(cars, "Audi");
         System.out.println("Audi cars found: " + audiCars.length);
+        for (Car car : audiCars) {
+            System.out.println(" - " + car.getBrand() + " " + car.getModel() + " " + car.getYear());}
+
+
     }
 
     public static Car[] getCarByBrend(Car[] cars, String brend) {
@@ -19,7 +25,7 @@ public class Main {
         int count = 0;
         for (Car car : cars) {
             if (car.getBrand().equalsIgnoreCase(brend)) {
-                count++;
+                count++;    
             }
         }
 
@@ -30,6 +36,33 @@ public class Main {
         // Заполняем массив
         for (Car car : cars) {
             if (car.getBrand().equalsIgnoreCase(brend)) {
+                result[index] = car;
+                index++;
+                //pizdes
+            }
+        }
+        for (int i = 0; i < result.length; ++i){
+            System.out.println(result[i]);
+        }
+        return result;
+    }
+    public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years) {
+        int currentYear = java.time.Year.now().getValue();
+        int count = 0;
+        
+        for (Car car : cars) {
+            if (car.getBrand().equalsIgnoreCase(brend) && 
+                (currentYear - car.getYear()) > years) {
+                count++;
+            }
+        }
+
+        Car[] result = new Car[count];
+        int index = 0;
+        
+        for (Car car : cars) {
+            if (car.getBrand().equalsIgnoreCase(brend) && 
+                (currentYear - car.getYear()) > years) {
                 result[index] = car;
                 index++;
             }
