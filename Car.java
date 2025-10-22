c026576e01fedba776b1e2199b2238578b01c46f
S <trdhgfukc@gmail.com>
2025-10-22 23:43:21 +0700

    добавлен getcarbybrend

--- Car.java
+++ Car.java
@@ -1,60 +1,77 @@
-public class Car{
+public class Car {
     private int id;
     private String brand;
     private String model;
     private int year;
     private String color;
-    private int price;
-    private String rgNum;
-}
-public Car(int id, String brand, String model,int year, String color, int price, String rgNum){
-    this.id = id;
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.color = color;
-    this.price = price;
-    this.rgNum = rgNum;
-}
-public setId(){
-    return id;
-}
-public void getId(){
-    this.id = id;
-}
-public void setBrand(){
-    return brand;
-}
-public void getBrand(){
-    this.brand = brand;
-}
-public void setModel(){
-    return model;
-}
-public void getModel(){
-    this.model = model;
-}
-public void setYear(){
-    return year;
-}
-public void getYear(){
-    this.year = year;
-}
-public void setColor(){
-    return color;
-}
-public void getColor(){
-    this.color = color;
-}
-public void getPrice(){
-    this.price = price;
-}
-public void setPrice(){
-    return price;
-}
-public void setRGNum(){
-    return rgNum;
-}
-public void getRGNum(){
-    this.rgNum = rgNum;
+    private double price;
+    private String registrationNumber;
+
+    // Конструктор
+    public Car(int id, String brand, String model, int year, String color, double price, String registrationNumber) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+        this.registrationNumber = registrationNumber;
+    }
+
+    // Геттеры и сеттеры
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
-public class Main{
-    public static void main(String[] argc){
+public class Main {
+    public static void main(String[] args) {
+        Car[] cars = {
+            new Car(1, "Toyota", "Camry", 2020, "Black", 25000, "AB123CD"),
+            new Car(2, "Honda", "Civic", 2019, "White", 22000, "EF456GH"),
+            new Car(3, "Toyota", "Corolla", 2021, "Red", 20000, "IJ789KL"),
+            new Car(4, "BMW", "X5", 2018, "Blue", 45000, "MN012OP")
+        };
+
+        Car[] toyotaCars = getCarByBrend(cars, "Toyota");
+        System.out.println("Toyota cars found: " + toyotaCars.length);
         
+        Car[] audiCars = getCarByBrend(cars, "Audi");
+        System.out.println("Audi cars found: " + audiCars.length);
+    }
+
+    public static Car[] getCarByBrend(Car[] cars, String brend) {
+        // Считаем количество машин нужной марки
+        int count = 0;
+        for (Car car : cars) {
+            if (car.getBrand().equalsIgnoreCase(brend)) {
+                count++;
+            }
+        }
+
+        // Создаем массив нужного размера
+        Car[] result = new Car[count];
+        int index = 0;
+        
+        // Заполняем массив
+        for (Car car : cars) {
+            if (car.getBrand().equalsIgnoreCase(brend)) {
+                result[index] = car;
+                index++;
+            }
+        }
+        
+        return result;
     }
 }
